8d03904d12336ca573cd4bb27ca893c481ba6981
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-04-01 00:26:18 +0200

    ci: update build script steps

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,29 +1,31 @@
 ﻿param(
-    [string]$SettingsFile = "../config/local-build-settings.json"
+    [string]$SettingsFilePath = "../config/local-build-settings.json"
 )
 
-if (!(Test-Path $SettingsFile)) {
-    Write-Host "❌ Settings file: $SettingsFile not found"
+if (!(Test-Path $SettingsFilePath)) {
+    Write-Host "❌ Settings file: $SettingsFilePath not found"
     exit 1
 }
 
-$Settings = Get-Content $SettingsFile -Raw | ConvertFrom-Json
+#✅  
+$Settings = Get-Content $SettingsFilePath -Raw | ConvertFrom-Json
 
 $AbsoluteSolutionPath = (Resolve-Path -Path $Settings.SolutionRelativePath).Path
 $AbsoluteOutputPath   = (Resolve-Path -Path $Settings.OutputPath).Path
+
 $SolutionPath  = $Settings.SolutionRelativePath
 $OutputPath    = $Settings.OutputPath
 $Configuration = $Settings.Configuration
 
-Write-Host "Absolute Solution path: $AbsoluteSolutionPath"
+Write-Host "🧪 Absolute Solution path: $AbsoluteSolutionPath"
 
-Write-Host "Absolute Output path: $AbsoluteOutputPath"
+Write-Host "🧪 Absolute Output path: $AbsoluteOutputPath"
 
-Write-Host "[1/3] Restoring solution packages $Settings.SolutionRelativePath ..."
+Write-Host "⭐ [1/3] Restoring solution packages $Settings.SolutionRelativePath ..."
 dotnet restore $Settings.SolutionRelativePath
 
-Write-Host "[2/3] Building solution $SolutionPath ..."
+Write-Host "⭐ [2/3] Building solution $SolutionPath ..."
 dotnet build $SolutionPath --no-restore --configuration $Configuration
 
-Write-Host "[3/3] Packing solution $SolutionPath to $AbsoluteOutputPath ..."
+Write-Host "⭐ [3/3] Packing to $AbsoluteOutputPath ..."
 dotnet pack $SolutionPath --no-build --configuration $Configuration --output $OutputPath
