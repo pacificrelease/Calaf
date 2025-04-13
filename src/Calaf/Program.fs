c1dad59bd39249079a99dfaa2f7601c1b8de9a5d
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-04-13 12:05:30 +0200

    fix: remove garbage from the path in the workspace creation workflow

--- src/Calaf/Program.fs
+++ src/Calaf/Program.fs
@@ -7,7 +7,7 @@ open Calaf
 let searchFilesPattern = "*.?sproj"
 
 let rootPath = "../../../../.."
-let result = Api.CreateWorkspace "dsdsddd" searchFilesPattern
+let result = Api.CreateWorkspace null searchFilesPattern
 
 match result with
 | Error error ->
