5e1958e700ce6545a6854e78ba4be5019ef8d17a
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-04-16 23:58:01 +0200

    test: add more tests for Patch TryParseFromString function

--- tests/Calaf.Tests/PatchTests.fs
+++ tests/Calaf.Tests/PatchTests.fs
@@ -1,13 +1,14 @@
-﻿module Calaf.Tests.PatchTests
+﻿namespace Calaf.Tests.PatchTests
 
 open FsCheck.Xunit
+open Xunit
 
 open Calaf.Domain.Patch
 open Calaf.Tests
 
 module TryParseFromStringPropertyTests =
-    [<Property>]
-    let ``Valid uint32 string parse to corresponding value`` (validPatch: uint32) =
+    [<Property(Arbitrary = [| typeof<Arbitrary.greaterThanZeroUInt32> |])>]
+    let ``Valid uint32 greater than zero string parse to corresponding value`` (validPatch: uint32) =
         validPatch
         |> string
         |> tryParseFromString  = Some validPatch
@@ -17,3 +18,21 @@ module TryParseFromStringPropertyTests =
         nonNumberPatchStr
         |> string
         |> tryParseFromString= None
+        
+    [<Property(Arbitrary = [| typeof<Arbitrary.overflowUInt32String> |])>]
+    let ``Number out of uint32 range parse to None`` (overflowPatch: string) =
+        overflowPatch
+        |> tryParseFromString= None
+        
+module TryParseFromStringTests =
+    [<Fact>]
+    let ``Empty string returns None`` () =
+        tryParseFromString "" = None
+        
+    [<Fact>]
+    let ``Whitespace returns None`` () =
+        tryParseFromString " " = None
+        
+    [<Fact>]
+    let ``Zero return None`` () =
+        tryParseFromString "0" = None
--- tests/Calaf.Tests/TestHelper.fs
+++ tests/Calaf.Tests/TestHelper.fs
@@ -2,7 +2,21 @@
 
 open FsCheck.FSharp
 
-module Generator =         
+module Generator =
+    
+    
+    
+    let greaterThanZeroUInt32 =
+        gen {
+            let! hi = Gen.choose(0, 0xFFFF)
+            let! lo = Gen.choose(0, 0xFFFF)
+            let combined = (uint32 hi <<< 16) ||| uint32 lo
+            if combined = 0u then
+                return 1u
+            else
+                return combined
+        }
+        
     let nonNumericString =
         let letterChars = ['a'..'z'] @ ['A'..'Z']
         let specialChars = ['!'; '@'; '#'; '$'; '%'; '^'; '&'; '*'; '('; ')'; '-'; '_'; '+'; '='; '~'; '?'; '/'; '\\'; '['; ']'; '{'; '}'; '|'; '<'; '>'; ','; '.'; ':']
@@ -15,20 +29,59 @@ module Generator =
                 return string(charArray)
             }
             
+        let genGarbage =
+            gen {
+                let! garbage = Gen.elements ["NaN"; "Infinity"; "-Infinity"]
+                return garbage
+            }
+            
         gen {
             let! choice = Gen.frequency [
-                4, genStringFromChars letterChars             // alphabetic strings
-                3, genStringFromChars letterOrSpecialChars    // alphabetic + special
-                1, gen {
-                    let! intPart = Gen.choose(-1000, 1000)
-                    let! fracPart = Gen.choose(0, 999999)
-                    return $"{intPart}.{fracPart:D6}"
-                }
+                4, genStringFromChars letterChars
+                3, genStringFromChars letterOrSpecialChars      
+                1, genGarbage
+            ]
+            return choice
+        }
+        
+    let overflowUInt32String =
+        let genTooBig =
+            gen {        
+                let! big = Gen.choose64(int64 System.UInt32.MaxValue + 1L, System.Int64.MaxValue)
+                return big |> string
+            }
+
+        let genNegative =
+            gen {
+                let! neg = Gen.choose(-99999, -1)
+                return neg |> string
+            }
+
+        let genFloat =
+            gen {
+                let! whole = Gen.choose(0, 10000)
+                let! frac = Gen.choose(1, 999999)
+                return $"{whole}.{frac:D6}"
+            }        
+        
+        gen {
+            let! choice = Gen.frequency [
+                3, genTooBig
+                2, genNegative
+                1, genFloat
             ]
             return choice
         }
 
-module Arbitrary =            
+module Arbitrary =
+    type greaterThanZeroUInt32 =
+        static member greaterThanZeroUInt32() =
+            Arb.fromGen Generator.greaterThanZeroUInt32
+            
     type internal nonNumericString =
-        static member NonNumericString() =
+        static member nonNumericString() =
             Arb.fromGen Generator.nonNumericString
+            
+    type internal overflowUInt32String =
+        static member overflowUInt32String() =
+            Arb.fromGen Generator.overflowUInt32String
