0afea096cb169a11e04081576a03a2d471345e0d
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-04-17 22:37:50 +0200

    refactor: change internal structure of Month functions

--- src/Calaf/Domain/Month.fs
+++ src/Calaf/Domain/Month.fs
@@ -2,16 +2,20 @@
 
 open Calaf.Domain.DomainTypes
 
+let private validate (month: System.Byte) : Month option =
+    match month with
+    | month when month >= byte 1 && month <= byte 12 -> Some month
+    | _ -> None
+
 let tryParseFromInt32 (month: System.Int32) : Month option =
     try
-        let month = System.Convert.ToByte(month)
-        Some month
-    with _ ->
-        None
+        month
+        |> System.Convert.ToByte
+        |> validate
+    with _ -> None
         
 let tryParseFromString (month: string) : Month option =
-    match System.Byte.TryParse(month) with
-    | true, month when month >= byte 1 && month <= byte 12 ->
-        Some month
-    | _ ->
-        None
+    match month |> System.Byte.TryParse with
+    | true, month ->
+        month |> validate
+    | _ -> None
