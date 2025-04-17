32969b9e08d6bc89f3d5db69fe48ed4df01d0783
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-04-17 18:04:37 +0200

    refactor: change the Month tryParseFromString function workflow

--- src/Calaf/Domain/Month.fs
+++ src/Calaf/Domain/Month.fs
@@ -1,24 +1,7 @@
 ﻿module internal Calaf.Domain.Month
 
-open FsToolkit.ErrorHandling
-
 open Calaf.Domain.DomainTypes
 
-let private tryParseMonthString (month: string) : SuitableVersionPart option =
-    match month with
-    | month when (month.Length = 1 || month.Length = 2) &&
-                  month |> Seq.forall System.Char.IsDigit
-        -> Some month
-    | _ -> None
-    
-let private tryParseMonth (suitableMonthString: SuitableVersionPart) : Month option =
-    match System.Byte.TryParse(suitableMonthString) with
-    | true, month when month > byte 1 && month <= byte 12 ->
-        Some month
-    | _ ->
-        None
-    
-    
 let tryParseFromInt32 (month: System.Int32) : Month option =
     try
         let month = System.Convert.ToByte(month)
@@ -27,7 +10,8 @@ let tryParseFromInt32 (month: System.Int32) : Month option =
         None
         
 let tryParseFromString (month: string) : Month option =
-    option {            
-        let! suitableMonthString = tryParseMonthString(month)                
-        return! tryParseMonth suitableMonthString
-    }
+    match System.Byte.TryParse(month) with
+    | true, month when month >= byte 1 && month <= byte 12 ->
+        Some month
+    | _ ->
+        None
