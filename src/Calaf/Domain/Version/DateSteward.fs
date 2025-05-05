2c1f00bce90569e61ea41b7729f8c86fbb64772f
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-05-05 22:33:17 +0200

    feat: add tryCreate DateStamp to DateSteward module

--- src/Calaf/Domain/Version/DateSteward.fs
+++ src/Calaf/Domain/Version/DateSteward.fs
@@ -1,4 +1,13 @@
 module internal Calaf.Domain.DateSteward
 
-// let tryCreate (bareDateTime: System.DateTime) =
-//     let! 
+open FsToolkit.ErrorHandling
+
+open Calaf.Domain.Errors
+open Calaf.Domain.DomainTypes
+
+let tryCreate (bareDateTime: System.DateTime) : Result<DateStamp, CalafError> =
+    result {
+        let! year  = Year.tryParseFromInt32 bareDateTime.Year
+        let! month = Month.tryParseFromInt32 bareDateTime.Month
+        return { Year = year; Month = month }        
+    }
