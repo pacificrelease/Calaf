ff2f7a9215793f563a66e6c0f4546f284c610c14
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-05-11 16:42:12 +0200

    feat: add bump Suite function

--- src/Calaf/Domain/Suite/Suite.fs
+++ src/Calaf/Domain/Suite/Suite.fs
@@ -1,8 +1,27 @@
 module internal Calaf.Domain.Suite
 
+open FsToolkit.ErrorHandling
+
 open Calaf.Domain.DomainTypes
+open Calaf.Domain.Project
 
+let bump (suite: Suite) (nextVersion: CalendarVersion) =
+    result {
+        let! bumpedProjects =
+            suite.Projects
+            |> Array.traverseResultM (fun project -> 
+                match project with
+                | Versioned { Version = CalVer _ } as p ->
+                    tryBump p nextVersion
+                | project ->
+                    Ok project)
+                
+        return { 
+            Version = Some nextVersion
+            Projects = bumpedProjects 
+        }
+    }
+    
 let create (projects: Project[]) : Suite =
-    { Version  = projects |> Project.chooseCalendarVersions |> Version.tryMax
+    { Version  = projects |> chooseCalendarVersions |> Version.tryMax
       Projects = projects }
-
