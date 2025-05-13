18c81d86b3fb276ef2bb8a268f46e9699eea2516
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-05-13 23:40:40 +0200

    feat: add tryBump Repository function

--- src/Calaf/Domain/Git/Repository.fs
+++ src/Calaf/Domain/Git/Repository.fs
@@ -4,6 +4,7 @@ open FsToolkit.ErrorHandling
 
 open Calaf.Contracts
 open Calaf.Domain.DomainTypes
+open Calaf.Domain.DomainEvents
 
 let tryCreate (repoInfo: GitRepositoryInfo) =
     result {
@@ -41,4 +42,30 @@ let tryCreate (repoInfo: GitRepositoryInfo) =
             
         | _ ->
             return Damaged repoInfo.Directory
+    }
+
+let tryBump (repo: Repository) (nextVersion: CalendarVersion) =
+    result {
+        match repo with        
+        | Ready (dir, head, signature, currentVersion) ->
+            let sameVersion = currentVersion
+                            |> Option.map (fun v -> v = nextVersion)
+                            |> Option.defaultValue false
+            if sameVersion
+            then
+                return! CurrentRepository |> Error
+            else
+                let event = ReadyRepositoryBumped {
+                    Version = nextVersion
+                    Signature = signature
+                }
+                return Ready (dir, head, signature, Some nextVersion), event                
+        | Dirty _ ->
+            return! DirtyRepository |> Error
+        | Unborn _ ->
+            return! UnbornRepository |> Error
+        | Unsigned _ ->
+            return! UnsignedRepository |> Error
+        | Damaged _ ->
+            return! DamagedRepository |> Error
     }
