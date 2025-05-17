b082c415de1ce6395cd65f56ea445e74aa6ada1d
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-05-17 13:13:51 +0200

    refactor: change structure and naming in Patch module

--- src/Calaf/Domain/Version/Patch.fs
+++ src/Calaf/Domain/Version/Patch.fs
@@ -1,15 +1,15 @@
 ﻿module internal Calaf.Domain.Patch
 
 open Calaf.Domain.DomainTypes
-
-let bump (patch: Patch option) : Patch =
-    let increment = 1u
-    match patch with
-    | Some p when p < Patch.MaxValue -> p + increment
-    | Some _ -> increment
-    | None -> increment
     
 let tryParseFromString (patch: string) : Patch option =
     match System.UInt32.TryParse(patch) with
     | true, patch when patch > 0u -> Some patch
     | _ -> None
+    
+let bump (patch: Patch option) : Patch =
+    let incrementStep = 1u
+    match patch with
+    | Some p when p < Patch.MaxValue -> p + incrementStep
+    | Some _ -> incrementStep
+    | None -> incrementStep
