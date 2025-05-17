e8b03cf845df4c0a784b46ccaa78fe247f862f3e
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-05-17 13:12:29 +0200

    refactor: change structure in Tag module

--- src/Calaf/Domain/Git/Tag.fs
+++ src/Calaf/Domain/Git/Tag.fs
@@ -3,12 +3,6 @@
 open Calaf.Contracts
 open Calaf.Domain.DomainTypes
 
-let chooseCalendarVersions (tags: Tag seq) : CalendarVersion seq =
-    tags
-    |> Seq.choose (function
-        | Tag.Versioned (_, CalVer version, _) -> Some version
-        | _ -> None)
-
 let create (tagInfo: GitTagInfo) =
     match Version.tryParseFromTag tagInfo.Name with
     | Some v when v.IsCalVer || v.IsSemVer ->
@@ -16,3 +10,9 @@ let create (tagInfo: GitTagInfo) =
         Tag.Versioned (tagInfo.Name, v, commit)
     | _ ->
         Tag.Unversioned tagInfo.Name
+        
+let chooseCalendarVersions (tags: Tag seq) : CalendarVersion seq =
+    tags
+    |> Seq.choose (function
+        | Tag.Versioned (_, CalVer version, _) -> Some version
+        | _ -> None)
