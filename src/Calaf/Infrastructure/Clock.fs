7752616c8c52178524937e76c376842c08e2a0f3
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-05-21 13:52:16 +0200

    fix: correct Clock module declaration

--- src/Calaf/Infrastructure/Clock.fs
+++ src/Calaf/Infrastructure/Clock.fs
@@ -3,6 +3,7 @@ namespace Calaf.Infrastructure
 
 open System
 
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module internal Clock =
     let now () =
         DateTimeOffset.Now
