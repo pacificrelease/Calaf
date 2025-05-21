ef971f9123f0a1236921fc9d46d2ffd0337053a5
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-05-21 18:57:09 +0200

    refactor: implement FileSystem adapter

--- src/Calaf/Adapters.fs
+++ src/Calaf/Adapters.fs
@@ -3,7 +3,7 @@ namespace Calaf.Infrastructure
 open Calaf.Contracts
 open Calaf.Application
 
-module internal GitRepository =    
+module internal Repository =    
     open LibGit2Sharp   
     
     let read
@@ -82,26 +82,119 @@ module internal GitRepository =
         with exn ->
             exn |> RepoAccessFailed |> Git |> Error
 
+module internal File =
+    let load
+        (absolutePath: string) : Result<System.Xml.Linq.XElement, InfrastructureError> =
+        try
+            let settings = System.Xml.XmlReaderSettings()
+            settings.DtdProcessing <- System.Xml.DtdProcessing.Prohibit
+            settings.XmlResolver <- null
+            use reader = System.Xml.XmlReader.Create(absolutePath, settings)
+            let xml = System.Xml.Linq.XElement.Load(reader)            
+            xml |> Ok
+        with
+        | :? System.UnauthorizedAccessException as exn ->
+            (absolutePath, exn :> System.Exception)
+            |> FileAccessDenied
+            |> FileSystem
+            |> Error
+        | exn ->
+            (absolutePath, exn)
+            |> XmlLoadFailed
+            |> FileSystem
+            |> Error
+    
+    let save
+        (absolutePath: string)
+        (content: System.Xml.Linq.XElement) : Result<System.Xml.Linq.XElement, InfrastructureError> =
+        try        
+            let options = System.Xml.Linq.SaveOptions.None
+            content.Save(absolutePath, options)
+            content |> Ok
+        with
+        | :? System.UnauthorizedAccessException as exn ->
+            (absolutePath, exn :> System.Exception)
+            |> FileAccessDenied
+            |> FileSystem
+            |> Error
+        | exn ->
+            (absolutePath, exn)
+            |> XmlSaveFailed
+            |> FileSystem
+            |> Error
+    
+module internal Directory =
+    open FsToolkit.ErrorHandling
+    open System.IO
+    
+    open Calaf.Extensions.InternalExtensions
+
+    let private find
+        (directory: DirectoryInfo)
+        (pattern: string) =
+        try
+            directory.GetFiles(pattern, SearchOption.AllDirectories)
+            |> Array.toList
+            |> Ok
+        with exn ->
+            exn
+            |> FilesScanFailed
+            |> FileSystem
+            |> Error        
+
+    let private info
+        (path: string) =
+        try
+            let path = DirectoryInfo path
+            if path.Exists
+            then path |> Ok
+            else DirectoryDoesNotExist
+                 |> FileSystem
+                 |> Error
+        with exn ->
+            exn
+            |> DirectoryAccessDenied
+            |> FileSystem
+            |> Error
+            
+    let list
+        (path: string)
+        (pattern: string) =
+        result {
+            let! dirInfo = info path
+            let! files = find dirInfo pattern
+            let projects, _ =
+                files
+               |> List.map (fun fileInfo -> File.load fileInfo.FullName
+                                            |> Result.map (fun xml ->  fileInfo, xml))
+               |> Result.partition                                   
+            return Mappings.toWorkspaceDirectoryInfo dirInfo projects
+        }
+
 type Git() =
     interface IGit with
         member _.tryRead directory maxTagsToRead timeStamp =
-            GitRepository.read directory maxTagsToRead timeStamp |> Result.mapError CalafError.Infrastructure
+            Repository.read directory maxTagsToRead timeStamp
+            |> Result.mapError CalafError.Infrastructure
             
         member _.tryApply directory commitMessage tagName signature =
-            GitRepository.apply directory commitMessage tagName signature |> Result.mapError CalafError.Infrastructure
-
-
+            Repository.apply directory commitMessage tagName signature
+            |> Result.mapError CalafError.Infrastructure
 
 type FileSystem() =
     interface IFileSystem with
-        member this.tryReadDirectory directory pattern =
-            failwith "todo"
+        member _.tryReadDirectory directory pattern =
+            Directory.list directory pattern
+            |> Result.mapError CalafError.Infrastructure
             
-        member this.tryReadXml absolutePath =
-            failwith "todo"
+        member _.tryReadXml absolutePath =
+            File.load absolutePath
+            |> Result.mapError CalafError.Infrastructure
             
-        member this.tryWriteXml absolutePath content =
-            failwith "todo"
+        member _.tryWriteXml absolutePath content =
+            File.save absolutePath content
+            |> Result.mapError CalafError.Infrastructure
+            |> Result.map ignore
             
 type Clock() =
     interface IClock with
--- src/Calaf/Application/Errors.fs
+++ src/Calaf/Application/Errors.fs
@@ -1,7 +1,22 @@
 namespace Calaf.Application
 
 open Calaf.Domain
-open Calaf.Infrastructure
+
+type GitError =
+    | RepoNotInitialized
+    | RepoAccessFailed of ex: exn    
+    
+type FileSystemError =
+    | DirectoryDoesNotExist
+    | DirectoryAccessDenied of ex: exn
+    | FileAccessDenied      of absolutePath: string * ex: exn
+    | FilesScanFailed       of ex: exn
+    | XmlLoadFailed         of absolutePath: string * ex: exn
+    | XmlSaveFailed         of absolutePath: string * ex: exn
+    
+type InfrastructureError =
+    | Git        of GitError
+    | FileSystem of FileSystemError
 
 type CalafError =
     | Domain         of DomainError
