ec79e34c80ea20ea2c725ff67a4f77c3480fb003
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-05-23 11:43:37 +0200

    refactor: move and split FileSystem infrastructure

---
+++ src/Calaf/Infrastructure/Adapters/FileSystem.fs
@@ -0,0 +1,133 @@
+namespace Calaf.Infrastructure
+
+open Calaf.Contracts
+open Calaf.Application
+
+module internal FileSystemGateway =
+    let toProjectXmlFileInfo
+        (file: System.IO.FileInfo)
+        (xml: System.Xml.Linq.XElement) : ProjectXmlFileInfo =
+        { Name = file.Name
+          Directory = file.DirectoryName
+          Extension = file.Extension
+          AbsolutePath = file.FullName
+          Content = xml }
+        
+    let toWorkspaceDirectoryInfo
+        (directoryInfo: System.IO.DirectoryInfo)
+        (projects: (System.IO.FileInfo * System.Xml.Linq.XElement) seq) : DirectoryInfo =        
+        { Directory = directoryInfo.FullName
+          Projects = projects
+            |> Seq.map (fun (fileInfo, xml) -> toProjectXmlFileInfo fileInfo xml)
+            |> Seq.toList }
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module internal FileSystem =    
+    module Xml =
+        let load
+            (absolutePath: string) : Result<System.Xml.Linq.XElement, InfrastructureError> =
+            try
+                let settings = System.Xml.XmlReaderSettings()
+                settings.DtdProcessing <- System.Xml.DtdProcessing.Prohibit
+                settings.XmlResolver <- null
+                use reader = System.Xml.XmlReader.Create(absolutePath, settings)
+                let xml = System.Xml.Linq.XElement.Load(reader)            
+                xml |> Ok
+            with
+            | :? System.UnauthorizedAccessException as exn ->
+                (absolutePath, exn :> System.Exception)
+                |> FileAccessDenied
+                |> FileSystem
+                |> Error
+            | exn ->
+                (absolutePath, exn)
+                |> XmlLoadFailed
+                |> FileSystem
+                |> Error
+
+        let save
+            (absolutePath: string)
+            (content: System.Xml.Linq.XElement) : Result<unit, InfrastructureError> =
+            try        
+                let options = System.Xml.Linq.SaveOptions.None
+                content.Save(absolutePath, options) |> Ok
+            with
+            | :? System.UnauthorizedAccessException as exn ->
+                (absolutePath, exn :> System.Exception)
+                |> FileAccessDenied
+                |> FileSystem
+                |> Error
+            | exn ->
+                (absolutePath, exn)
+                |> XmlSaveFailed
+                |> FileSystem
+                |> Error
+                
+    module Directory =
+        open FsToolkit.ErrorHandling
+        open System.IO
+        
+        open Calaf.Extensions.InternalExtensions
+
+        let private find
+            (directory: DirectoryInfo)
+            (pattern: string) =
+            try
+                directory.GetFiles(pattern, SearchOption.AllDirectories)
+                |> Array.toList
+                |> Ok
+            with exn ->
+                exn
+                |> FilesScanFailed
+                |> FileSystem
+                |> Error        
+
+        let private info
+            (path: string) =
+            try
+                let path = DirectoryInfo path
+                if path.Exists
+                then path |> Ok
+                else DirectoryDoesNotExist
+                     |> FileSystem
+                     |> Error
+            with exn ->
+                exn
+                |> DirectoryAccessDenied
+                |> FileSystem
+                |> Error
+                
+        let private load
+            (fileInfo: FileInfo) =
+            result {
+                let! xml = Xml.load fileInfo.FullName
+                return fileInfo, xml
+            }
+                
+        let list
+            (path: string)
+            (pattern: string) =
+            result {
+                let! dirInfo = info path
+                let! files = find dirInfo pattern
+                let projects, _ =
+                    files
+                   |> List.map load
+                   |> Result.partition                                   
+                return FileSystemGateway.toWorkspaceDirectoryInfo dirInfo projects
+            }
+            
+type FileSystem() =
+    interface IFileSystem with
+        member _.tryReadDirectory directory pattern =
+            FileSystem.Directory.list directory pattern
+            |> Result.mapError CalafError.Infrastructure
+            
+        member _.tryReadXml absolutePath =
+            FileSystem.Xml.load absolutePath
+            |> Result.mapError CalafError.Infrastructure
+            
+        member _.tryWriteXml (absolutePath, content) =
+            FileSystem.Xml.save absolutePath content
+            |> Result.mapError CalafError.Infrastructure
+            |> Result.map ignore
