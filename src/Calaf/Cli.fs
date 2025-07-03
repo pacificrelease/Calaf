a8a220802966030c50e167d5ae98b1e606f0e610
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-07-03 22:50:02 +0200

    refactor: start dedicating Cli layer

---
+++ src/Calaf/Cli.fs
@@ -0,0 +1,58 @@
+﻿namespace Calaf
+
+open Argu
+
+open Calaf.Contracts
+
+type private MakeFlag =    
+    | [<CliPrefix(CliPrefix.None)>] Stable
+    | [<CliPrefix(CliPrefix.None)>] Nightly
+    interface IArgParserTemplate with
+        member flag.Usage =
+            match flag with
+            | Stable  -> "Make a stable version"
+            | Nightly -> "Make a nightly version"
+
+type private InputCommand = 
+    | [<SubCommand; CliPrefix(CliPrefix.None)>] Make of ParseResults<MakeFlag>
+    interface IArgParserTemplate with
+        member command.Usage =
+            match command with
+            | Make _ -> "Make a workspace version."
+            
+type CliError =
+    | BuildFlagNotRecognized of string
+    | CommandNotRecognized of string
+    
+module internal Cli =
+    let private createBuildFlagNotRecognized (flag: string) : Response =
+        { ExitCode = 1
+          Text = $"Build flag '{flag}' is not recognized." }
+        
+    let private createCommandNotRecognized (command: string) : Response =
+        { ExitCode = 1
+          Text = $"Command '{command}' is not recognized." }
+    
+    let private reduceMakeFlags (flags: MakeFlag list) =
+        match flags with
+        | [ Nightly ] -> Ok MakeType.Nightly
+        | [ Stable ]  -> Ok MakeType.Stable
+        | [] -> Ok MakeType.Stable
+        | _  ->
+            $"{flags.Head}" |> createBuildFlagNotRecognized |> Error   
+                
+    let private toCommand (results: ParseResults<InputCommand>) =
+        match results.GetAllResults() with
+        | [ Make makeFlagsResults ] ->
+            let makeFlags = makeFlagsResults.GetAllResults()
+            makeFlags |> reduceMakeFlags |> Result.map Command.Make
+        | [] -> MakeType.Stable |> Command.Make |> Ok
+        | commands ->
+            $"{commands.Head}" |> createCommandNotRecognized |> Error
+                
+    let private parse (args: string[]) =
+        let parser = ArgumentParser.Create<InputCommand>()
+        parser.ParseCommandLine args
+    
+    let command (args: string[]) =
+        args |> parse |> toCommand
---
+++ src/Calaf/Response.fs
@@ -0,0 +1,18 @@
+﻿namespace Calaf
+
+// Summaries
+type ReleaseSummary = {    
+    Directory: string
+    VersionedProjectsCount: int
+    TotalProjectsCount: int
+    UsesGit: bool
+    PreviousRelease: string
+    CurrentRelease: string
+}
+
+
+// Response
+type internal Response = {
+    ExitCode: int
+    Text: string
+}
