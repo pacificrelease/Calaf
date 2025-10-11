56bda412520ee976f251c40589c981ce03575679
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-10-11 23:48:43 +0200

    refactor: add 'Deps' to application layer for 'Make' use case

---
+++ src/Calaf/Application/UseCases/Deps.fs
@@ -0,0 +1,39 @@
+﻿namespace Calaf.Application.UseCases.Make
+
+open Calaf.Contracts
+open Calaf.Application
+
+type Deps = {
+    UtcNow:
+        unit ->
+        System.DateTime
+    
+    TryReadDirectory:
+        string ->
+        string list ->
+        string ->
+        Result<DirectoryInfo, CalafError>        
+    TryWriteXml:
+        string * System.Xml.Linq.XElement ->
+        Result<unit, CalafError>        
+    TryWriteMarkdown:
+        string * string ->
+        Result<unit, CalafError>
+        
+    TryGetRepo:
+        string ->
+        byte ->
+        string list ->
+        string list option ->
+        System.DateTimeOffset ->
+        Result<GitRepositoryInfo option, CalafError>        
+    TryListCommits:
+        string ->
+        string option ->
+        Result<GitCommitInfo list, CalafError>
+    TryApply:
+        string * string list ->
+        string ->
+        string ->
+        Result<unit, CalafError>
+}
