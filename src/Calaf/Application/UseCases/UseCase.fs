7fcaf492c44e66ef855775a15a9562e6f389735b
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-10-26 11:01:00 +0100

    refactor: cleanup and adjust code

--- src/Calaf/Application/UseCases/UseCase.fs
+++ src/Calaf/Application/UseCases/UseCase.fs
@@ -11,80 +11,81 @@ let private computeTagsExclude includePreRelease =
     then Version.preReleases
     else List.Empty
 
-let internal execute
-    (spec: MakeSpec)
-    (config: MakeConfiguration)
-    (deps: Deps) : Result<Workspace, CalafError> = result {
-    let timeStamp = deps.UtcNow()
-
-    // TODO: Dedicate domain to return projects search pattern (from policy) and projects paths (they can be both directory or files) if exist. When none return search pattern (from policy)
-    let targetProjects =
-        match spec.TargetProjects with
-        | Some p -> p |> List.map _.FullPath
-        | None -> []
-    let! dir = deps.TryReadDirectory spec.WorkspaceDirectory targetProjects config.ChangelogFileName
-    let tagsInclude = Version.versionPrefixes
-    let tagsExclude = spec.ChangelogGeneration |> Option.map (fun cg -> computeTagsExclude cg.IncludePreRelease)
-
-    let! repo = deps.TryGetRepo spec.WorkspaceDirectory config.TagsTake tagsInclude tagsExclude timeStamp
-
-    let! workspace, _ =
-        Workspace.tryCapture (dir, repo)
-        |> Result.mapError CalafError.Domain
-
-    let! nextVersion =
-        let make =
-            match spec.VersionType with
-            | VersionType.Nightly -> Version.tryNightly
-            | VersionType.Alpha -> Version.tryAlpha
-            | VersionType.Beta -> Version.tryBeta
-            | VersionType.ReleaseCandidate -> Version.tryReleaseCandidate
-            | VersionType.Stable -> Version.tryStable
-        make workspace.Version timeStamp |> Result.mapError CalafError.Domain
-
-    let! commitsOpt =
-        match spec.ChangelogGeneration with
-        | Some { IncludePreRelease = true } ->        
-            match workspace.Repository with
-            | Some (Dirty (_, { BaselineVersion = Some { TagName = tag; Version = CalVer { Build = None } } }))
-            | Some (Ready (_, { BaselineVersion = Some { TagName = tag; Version = CalVer { Build = None } } })) ->
-                deps.TryListCommits workspace.Directory (Some tag) |> Result.map Some
-            | Some (Dirty (_, { BaselineVersion = None }))
-            | Some (Ready (_, { BaselineVersion = None })) ->
-                deps.TryListCommits workspace.Directory None |> Result.map Some
-            | _ -> Ok None
-        | Some { IncludePreRelease = false } ->        
-            match workspace.Repository with
-            | Some (Dirty (_, { BaselineVersion = Some { TagName = tag; Version = CalVer { Build = Some _ } } }))
-            | Some (Ready (_, { BaselineVersion = Some { TagName = tag; Version = CalVer { Build = Some _ } } })) ->
-                deps.TryListCommits workspace.Directory (Some tag) |> Result.map Some
-            | Some (Dirty (_, { BaselineVersion = None }))
-            | Some (Ready (_, { BaselineVersion = None })) ->
-                deps.TryListCommits workspace.Directory None |> Result.map Some
-            | _ -> Ok None
-        |_ -> Ok None
-
-    let changesetOpt =
-        commitsOpt
-        |> Option.bind (fun commits ->
-            ReleaseNotes.tryCreate (commits |> List.map Commit.create) nextVersion timeStamp)
-
-    let! workspace', _ =
-        nextVersion |> Workspace.tryRelease workspace |> Result.mapError CalafError.Domain
-
-    let snapshot = Workspace.snapshot workspace' (changesetOpt |> Option.map fst)
-
-    do! snapshot.Changelog
-        |> Option.map (fun s -> deps.TryWriteMarkdown (s.AbsolutePath, s.ReleaseNotesContent))
-        |> Option.defaultValue (Ok ())
-
-    do! snapshot.Projects
-        |> List.traverseResultM (fun s -> deps.TryWriteXml (s.AbsolutePath, s.Content))
-        |> Result.map ignore
-
-    do! snapshot.Repository
-        |> Option.map (fun s -> deps.TryCreateCommit (s.Directory, s.PendingFilesPaths) s.CommitText s.TagName)
-        |> Option.defaultValue (Ok ())
-
-    return workspace'
-}
+// let internal execute
+//     (spec: MakeSpec)
+//     (config: MakeConfiguration)
+//     (deps: Deps) : Result<Workspace, CalafError> = result {
+//     let timeStamp = deps.UtcNow()
+//
+//     spec.ProjectsScope
+//     // TODO: Dedicate domain to return projects search pattern (from policy) and projects paths (they can be both directory or files) if exist. When none return search pattern (from policy)
+//     let targetProjects =
+//         match spec.TargetProjects with
+//         | Some p -> p |> List.map _.FullPath
+//         | None -> []
+//     let! dir = deps.TryReadDirectory spec.WorkspaceDirectory targetProjects config.ChangelogFileName
+//     let tagsInclude = Version.versionPrefixes
+//     let tagsExclude = spec.ChangelogGeneration |> Option.map (fun cg -> computeTagsExclude cg.IncludePreRelease)
+//
+//     let! repo = deps.TryGetRepo spec.WorkspaceDirectory config.TagsTake tagsInclude tagsExclude timeStamp
+//
+//     let! workspace, _ =
+//         Workspace.tryCapture (dir, repo)
+//         |> Result.mapError CalafError.Domain
+//
+//     let! nextVersion =
+//         let make =
+//             match spec.VersionType with
+//             | VersionType.Nightly -> Version.tryNightly
+//             | VersionType.Alpha -> Version.tryAlpha
+//             | VersionType.Beta -> Version.tryBeta
+//             | VersionType.ReleaseCandidate -> Version.tryReleaseCandidate
+//             | VersionType.Stable -> Version.tryStable
+//         make workspace.Version timeStamp |> Result.mapError CalafError.Domain
+//
+//     let! commitsOpt =
+//         match spec.ChangelogGeneration with
+//         | Some { IncludePreRelease = true } ->        
+//             match workspace.Repository with
+//             | Some (Dirty (_, { BaselineVersion = Some { TagName = tag; Version = CalVer { Build = None } } }))
+//             | Some (Ready (_, { BaselineVersion = Some { TagName = tag; Version = CalVer { Build = None } } })) ->
+//                 deps.TryListCommits workspace.Directory (Some tag) |> Result.map Some
+//             | Some (Dirty (_, { BaselineVersion = None }))
+//             | Some (Ready (_, { BaselineVersion = None })) ->
+//                 deps.TryListCommits workspace.Directory None |> Result.map Some
+//             | _ -> Ok None
+//         | Some { IncludePreRelease = false } ->        
+//             match workspace.Repository with
+//             | Some (Dirty (_, { BaselineVersion = Some { TagName = tag; Version = CalVer { Build = Some _ } } }))
+//             | Some (Ready (_, { BaselineVersion = Some { TagName = tag; Version = CalVer { Build = Some _ } } })) ->
+//                 deps.TryListCommits workspace.Directory (Some tag) |> Result.map Some
+//             | Some (Dirty (_, { BaselineVersion = None }))
+//             | Some (Ready (_, { BaselineVersion = None })) ->
+//                 deps.TryListCommits workspace.Directory None |> Result.map Some
+//             | _ -> Ok None
+//         |_ -> Ok None
+//
+//     let changesetOpt =
+//         commitsOpt
+//         |> Option.bind (fun commits ->
+//             ReleaseNotes.tryCreate (commits |> List.map Commit.create) nextVersion timeStamp)
+//
+//     let! workspace', _ =
+//         nextVersion |> Workspace.tryRelease workspace |> Result.mapError CalafError.Domain
+//
+//     let snapshot = Workspace.snapshot workspace' (changesetOpt |> Option.map fst)
+//
+//     do! snapshot.Changelog
+//         |> Option.map (fun s -> deps.TryWriteMarkdown (s.AbsolutePath, s.ReleaseNotesContent))
+//         |> Option.defaultValue (Ok ())
+//
+//     do! snapshot.Projects
+//         |> List.traverseResultM (fun s -> deps.TryWriteXml (s.AbsolutePath, s.Content))
+//         |> Result.map ignore
+//
+//     do! snapshot.Repository
+//         |> Option.map (fun s -> deps.TryCreateCommit (s.Directory, s.PendingFilesPaths) s.CommitText s.TagName)
+//         |> Option.defaultValue (Ok ())
+//
+//     return workspace'
+// }
--- src/Calaf/Cli/Mappers.fs
+++ src/Calaf/Cli/Mappers.fs
@@ -9,112 +9,112 @@ module Calaf.Arguments
         |> Guards.Workspace.getPathOrDefault
         |> Guards.Workspace.check
 
-    let private tryCommands (args: string[]) =
-        try
-            ArgumentParser.Create<InputCommand2>().ParseCommandLine(args) |> Ok
-        with
-        | exn ->
-            exn.Message
-            |> ArgumentsFatal
-            |> Input
-            |> CalafError.Infrastructure
-            |> Error
-            
-    let private tryDestructure (commands: MakeCommand2 list) =
-        let destruct (flags: ParseResults<MakeFlag2>) =
-            let changelog = flags.Contains Changelog
-            let includePreRelease = flags.Contains IncludePreRelease
-            let targetProjects =
-                flags.TryGetResult Projects
-                |> Option.defaultValue List.Empty
-                |> List.filter (fun p -> not (System.String.IsNullOrWhiteSpace p))
-                |> List.distinct            
-            if includePreRelease && not changelog then
-                ChangelogFlagRequired
-                |> Input
-                |> CalafError.Infrastructure
-                |> Error
-            else
-                Ok (changelog, includePreRelease, targetProjects)
-                
-        let combine versionType flags =
-            destruct flags
-            |> Result.map (fun (changelog, includePreRelease, targetProjects) -> 
-                (versionType, changelog, includePreRelease, targetProjects))
-            
-        match commands with
-            | [ MakeCommand2.Nightly nFlags ] -> combine VersionType.Nightly nFlags
-            | [ MakeCommand2.Alpha aFlags ] -> combine VersionType.Alpha aFlags
-            | [ MakeCommand2.Beta bFlags ] -> combine VersionType.Beta bFlags
-            | [ MakeCommand2.RC rcFlags ] -> combine VersionType.ReleaseCandidate rcFlags
-            | [ MakeCommand2.Stable sFlags ] -> combine VersionType.Stable sFlags
-            | [] ->               
-                MakeCommandMissing
-                |> Input
-                |> CalafError.Infrastructure
-                |> Error
-            | _  ->
-                $"{commands.Head}"
-                |> MakeCommandNotRecognized
-                |> Input
-                |> CalafError.Infrastructure
-                |> Error
-    
-    let private trySpec
-        (directory: ValidatedDirectoryFullPath)
-        (args: ParseResults<InputCommand2>) =
-        result {            
-            let cliArgs = args.GetAllResults()
-            match cliArgs with
-            | [ Make2 makeArgs ] ->
-                return!
-                    makeArgs.GetAllResults()
-                    |> tryDestructure
-                    |> Result.bind (fun (versionType, changelog, includePreRelease, projects) ->
-                        result {
-                            let! projects = Guards.Projects.check directory projects
-                            let (ValidatedDirectoryFullPath directory) = directory
-                            let projects =
-                                // Lets suppose that x is always full path make a record with full and relative path
-                                projects                                
-                                |> List.map (fun x -> { FullPath = x; RelativePath = System.IO.Path.GetRelativePath(directory, x) })                               
-                            
-                            let targetProjects =
-                                if projects.IsEmpty
-                                then None
-                                else Some projects
-                            let changelogGeneration =
-                                if changelog
-                                then Some { IncludePreRelease = includePreRelease }
-                                else None
-                            
-                            let makeSpec : MakeSpec =
-                                { WorkspaceDirectory = directory
-                                  VersionType = versionType
-                                  ChangelogGeneration = changelogGeneration
-                                  TargetProjects = targetProjects }
-                            return makeSpec                            
-                        })
-            | [] ->
-                let makeSpec : MakeSpec =
-                   let (ValidatedDirectoryFullPath directory) = directory
-                   { WorkspaceDirectory = directory
-                     VersionType = VersionType.Stable
-                     ChangelogGeneration = Some { IncludePreRelease = true }
-                     TargetProjects = None }
-                return makeSpec                                              
-            | commands ->
-                return!
-                    ($"{commands.Head}" |> CommandNotRecognized |> Input |> CalafError.Infrastructure)
-                    |> Error
-        }
-                
-    let tryParseToSpec (directory: string) (args: string[]) =
-        result {
-            let! workspace = tryWorkspace directory |> Result.mapError CliError.map
-            let! commands = tryCommands args |> Result.mapError CliError.map
-            let! spec =
-                trySpec workspace commands
-                |> Result.mapError CliError.map
-            return spec
-        }
+    // let private tryCommands (args: string[]) =
+    //     try
+    //         ArgumentParser.Create<InputCommand2>().ParseCommandLine(args) |> Ok
+    //     with
+    //     | exn ->
+    //         exn.Message
+    //         |> ArgumentsFatal
+    //         |> Input
+    //         |> CalafError.Infrastructure
+    //         |> Error
+    //         
+    // let private tryDestructure (commands: MakeCommand2 list) =
+    //     let destruct (flags: ParseResults<MakeFlag2>) =
+    //         let changelog = flags.Contains Changelog
+    //         let includePreRelease = flags.Contains IncludePreRelease
+    //         let targetProjects =
+    //             flags.TryGetResult Projects
+    //             |> Option.defaultValue List.Empty
+    //             |> List.filter (fun p -> not (System.String.IsNullOrWhiteSpace p))
+    //             |> List.distinct            
+    //         if includePreRelease && not changelog then
+    //             ChangelogFlagRequired
+    //             |> Input
+    //             |> CalafError.Infrastructure
+    //             |> Error
+    //         else
+    //             Ok (changelog, includePreRelease, targetProjects)
+    //             
+    //     let combine versionType flags =
+    //         destruct flags
+    //         |> Result.map (fun (changelog, includePreRelease, targetProjects) -> 
+    //             (versionType, changelog, includePreRelease, targetProjects))
+    //         
+    //     match commands with
+    //         | [ MakeCommand2.Nightly nFlags ] -> combine VersionType.Nightly nFlags
+    //         | [ MakeCommand2.Alpha aFlags ] -> combine VersionType.Alpha aFlags
+    //         | [ MakeCommand2.Beta bFlags ] -> combine VersionType.Beta bFlags
+    //         | [ MakeCommand2.RC rcFlags ] -> combine VersionType.ReleaseCandidate rcFlags
+    //         | [ MakeCommand2.Stable sFlags ] -> combine VersionType.Stable sFlags
+    //         | [] ->               
+    //             MakeCommandMissing
+    //             |> Input
+    //             |> CalafError.Infrastructure
+    //             |> Error
+    //         | _  ->
+    //             $"{commands.Head}"
+    //             |> MakeCommandNotRecognized
+    //             |> Input
+    //             |> CalafError.Infrastructure
+    //             |> Error
+    //
+    // let private trySpec
+    //     (directory: ValidatedDirectoryFullPath)
+    //     (args: ParseResults<InputCommand2>) =
+    //     result {            
+    //         let cliArgs = args.GetAllResults()
+    //         match cliArgs with
+    //         | [ Make2 makeArgs ] ->
+    //             return!
+    //                 makeArgs.GetAllResults()
+    //                 |> tryDestructure
+    //                 |> Result.bind (fun (versionType, changelog, includePreRelease, projects) ->
+    //                     result {
+    //                         let! projects = Guards.Projects.check directory projects
+    //                         let (ValidatedDirectoryFullPath directory) = directory
+    //                         let projects =
+    //                             // Lets suppose that x is always full path make a record with full and relative path
+    //                             projects                                
+    //                             |> List.map (fun x -> { FullPath = x; RelativePath = System.IO.Path.GetRelativePath(directory, x) })                               
+    //                         
+    //                         let targetProjects =
+    //                             if projects.IsEmpty
+    //                             then None
+    //                             else Some projects
+    //                         let changelogGeneration =
+    //                             if changelog
+    //                             then Some { IncludePreRelease = includePreRelease }
+    //                             else None
+    //                         
+    //                         let makeSpec : MakeSpec =
+    //                             { WorkspaceDirectory = directory
+    //                               VersionType = versionType
+    //                               ChangelogGeneration = changelogGeneration
+    //                               TargetProjects = targetProjects }
+    //                         return makeSpec                            
+    //                     })
+    //         | [] ->
+    //             let makeSpec : MakeSpec =
+    //                let (ValidatedDirectoryFullPath directory) = directory
+    //                { WorkspaceDirectory = directory
+    //                  VersionType = VersionType.Stable
+    //                  ChangelogGeneration = Some { IncludePreRelease = true }
+    //                  TargetProjects = None }
+    //             return makeSpec                                              
+    //         | commands ->
+    //             return!
+    //                 ($"{commands.Head}" |> CommandNotRecognized |> Input |> CalafError.Infrastructure)
+    //                 |> Error
+    //     }
+    //             
+    // let tryParseToSpec (directory: string) (args: string[]) =
+    //     result {
+    //         let! workspace = tryWorkspace directory |> Result.mapError CliError.map
+    //         let! commands = tryCommands args |> Result.mapError CliError.map
+    //         let! spec =
+    //             trySpec workspace commands
+    //             |> Result.mapError CliError.map
+    //         return spec
+    //     }
