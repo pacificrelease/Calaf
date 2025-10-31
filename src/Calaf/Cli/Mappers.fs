26513a4edf0684005cdb88f9e75c0016fe569cbe
Dmitrii Mikhailov <mikhailov.dv@outlook.com>
2025-10-31 23:36:41 +0100

    refactor: improve function formatting and add tryTargetProjectSpec

--- src/Calaf/Cli/Mappers.fs
+++ src/Calaf/Cli/Mappers.fs
@@ -20,8 +20,10 @@ module Calaf.Arguments
             |> CalafError.Infrastructure
             |> Error
             
-    let private tryDestructure (commands: MakeCommand2 list) =
-        let destruct (flags: ParseResults<MakeFlag2>) =
+    let private tryDestructure
+        (commands: MakeCommand2 list) =
+        let destruct
+            (flags: ParseResults<MakeFlag2>) =
             let changelog = flags.Contains Changelog
             let includePreRelease = flags.Contains IncludePreRelease
             let targetProjects =
@@ -29,7 +31,7 @@ module Calaf.Arguments
                 |> Option.defaultValue List.Empty
                 |> List.filter (fun p ->
                     not (System.String.IsNullOrWhiteSpace p))
-                |> List.distinct            
+                |> List.distinct
             if includePreRelease && not changelog then
                 ChangelogFlagRequired
                 |> Input
@@ -42,7 +44,9 @@ module Calaf.Arguments
                     TargetProjects = targetProjects |}
                 Ok destructuredArguments
                 
-        let combine releaseType flags =
+        let combine
+            releaseType
+            flags =
             destruct flags
             |> Result.map (fun a ->
                 {| ReleaseType = releaseType
@@ -67,6 +71,23 @@ module Calaf.Arguments
                 |> Input
                 |> CalafError.Infrastructure
                 |> Error
+                
+    let private tryTargetProjectSpec
+        (workspace: ValidatedDirectoryFullPath)
+        (project: ValidatedProjectFullPath)=
+        let (ValidatedDirectoryFullPath workspace) = workspace
+        let (ValidatedProjectFullPath project) = project
+        try
+            let relativePath =
+                (workspace, project)
+                |> System.IO.Path.GetRelativePath
+            Ok { AbsolutePath = project
+                 RelativePath = relativePath }
+        with
+        | _ ->
+            BadProjectPath project
+            |> CalafError.Validation
+            |> Error
     
     // let private trySpec
     //     (directory: ValidatedDirectoryFullPath)
@@ -78,14 +99,15 @@ module Calaf.Arguments
     //             return!
     //                 makeArgs.GetAllResults()
     //                 |> tryDestructure
-    //                 |> Result.bind (fun (versionType, changelog, includePreRelease, projects) ->
+    //                 |> Result.bind (fun a ->
     //                     result {
-    //                         let! projects = Guards.Projects.check directory projects
+    //                         let! projects = Guards.Projects.check directory a.TargetProjects
     //                         let (ValidatedDirectoryFullPath directory) = directory
     //                         let projects =
-    //                             // Lets suppose that x is always full path make a record with full and relative path
     //                             projects                                
-    //                             |> List.map (fun x -> { FullPath = x; RelativePath = System.IO.Path.GetRelativePath(directory, x) })                               
+    //                             |> List.map (fun x ->
+    //                                 { FullPath = x
+    //                                   RelativePath = System.IO.Path.GetRelativePath(directory, x) })                               
     //                         
     //                         let targetProjects =
     //                             if projects.IsEmpty
@@ -116,7 +138,7 @@ module Calaf.Arguments
     //                 ($"{commands.Head}" |> CommandNotRecognized |> Input |> CalafError.Infrastructure)
     //                 |> Error
     //     }
-    //             
+                
     // let tryParseToSpec (directory: string) (args: string[]) =
     //     result {
     //         let! workspace = tryWorkspace directory |> Result.mapError CliError.map
